b4c81ca5c692a2833eee6990b483ac0eeb5ad5c7
Jacint <jacint>
2020-12-04 18:57:30 +0000

    fix manifest and icons added

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,13 +3,15 @@ import React from "react";
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className="pa2">
-      <input
-        aria-label="search robots"
-        className="pa3 ba b--green bg-lightest-blue"
-        type="search"
-        placeholder="search robots"
-        onChange={searchChange}
-      />
+      <label>
+        <input
+          aria-label="search robots"
+          className="pa3 ba b--green bg-lightest-blue"
+          type="search"
+          placeholder="search robots"
+          onChange={searchChange}
+        />
+      </label>
     </div>
   );
 };
